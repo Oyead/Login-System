86be5e3e1a3e75a595d7acc1448d50579fe6d672
eyad <eyadswailam12345@gmail.com>
2024-12-14 19:14:31 +0200

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,135 +1,178 @@
-var loginBtn = document.getElementById('loginBtn')
-var signUpBtn = document.getElementById('signUpBtn')
-var logoutBtn=document.getElementById('logoutBtn')
+const navLinks = Array.from(document.querySelectorAll('.nav-item'));
+const btnFind = document.querySelector('#btnFind');
+const findInput = document.querySelector('#findInput');
+const forecast = document.querySelector('#forecast');
 
-var emailLogin = document.getElementById('emailLogin')
-var passwordLogin = document.getElementById('passwordLogin')
 
-var nameSignUp = document.getElementById('nameSignUp')
-var emailSignUp = document.getElementById('emailSignUp')
-var passwordSignUp = document.getElementById('passwordSignUp')
+const APIKey = '0504276985d1463eaa6155223233112';
+const baseURL = 'https://api.weatherapi.com/v1/forecast.json';
 
-var signUp = document.getElementById('signUp')
-var login = document.getElementById('login')
+let searchLocation = '';
+let weatherList = {};
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-var signUpMsg = document.getElementById('signUpMsg')
-var loginMsg = document.getElementById('loginMsg')
+getUserCity(); 
 
-var signUpCard = document.getElementById('signUpCard')
-var loginCard = document.getElementById('loginCard')
-var home=document.getElementById('home')
-
-login.addEventListener('click', showSignUp)
-signUp.addEventListener('click', showLogin)
-
-function showSignUp() {
-  loginCard.classList.add("d-none");
-  loginCard.classList.remove("d-flex");
-  signUpCard.classList.add("d-flex");
-  signUpCard.classList.remove("d-none");
-  home.classList.add("d-none")
-  home.classList.remove("d-flex")
-  loginMsg.innerHTML = '';
+function toggleCssClass(el, className, condition) {
+    condition ? el.classList.add(className) : el.classList.remove(className);
 }
 
-function showLogin() {
-  loginCard.classList.add("d-flex");
-  loginCard.classList.remove("d-none");
-  signUpCard.classList.add("d-none");
-  signUpCard.classList.remove("d-flex");
-  home.classList.add("d-none")
-  home.classList.remove("d-flex")
-  signUpMsg.innerHTML = '';
+function handleDay(dateString) {
+    let d = new Date(dateString);
+    let dayName = days[d.getDay()];
+    return dayName;
 }
-var welcomeName=document.getElementById('welcomeName')
-var username;
-function showHome(username) {
-  welcomeName.innerHTML='Welcome '
-  loginCard.classList.add("d-none");
-  loginCard.classList.remove("d-flex");
-  signUpCard.classList.add("d-none");
-  home.classList.remove("d-none");
-  home.classList.add("d-flex")
-  welcomeName.innerHTML+=username
+function handleMonth(dateString) {
+    let d = new Date(dateString);
+    let monthName = monthNames[d.getMonth()];
+    return monthName;
+}
+
+function normalizeCityName(cityName) {
+    return cityName.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
 
 
-var emailsArr;
+function getUserCity() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            try {
+                let response = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`)
+                let data = await response.json();
+                data.city ? searchLocation = normalizeCityName(data.city) : searchLocation = 'Assiut';
+                displayWeather();
+            } catch (err) {
+                console.log("Error fetching geolocation data:", err);
+                searchLocation = 'Assiut';
+                displayWeather();
 
-(function () {
-  if (localStorage.getItem('data') == null)
-    emailsArr = [];
-  else {
-    emailsArr = JSON.parse(localStorage.getItem('data'));
-  }
-})();
-
-signUpBtn.addEventListener('click', signUpAcc);
-
-function signUpAcc() {
-
-  if (signUpCheck()) {
-    signUpMsg.innerHTML = `<span style="color: rgb(40, 167, 69);">sucess</span>`;
-    var account = {
-      aName: nameSignUp.value,
-      aEmail: emailSignUp.value,
-      aPass: passwordSignUp.value
+            }
+        }, (error) => {
+            if (error.code === error.PERMISSION_DENIED) {
+                console.log("User denied the request for Geolocation.");
+            } else {
+                console.log("Error getting user location:", error.message);
+            }
+            searchLocation = 'Assiut';
+            displayWeather();
+        });
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+        searchLocation = 'Assiut';
+        displayWeather();
     }
-
-    emailsArr.push(account);
-    localStorage.setItem('data', JSON.stringify(emailsArr))
-  }
-
 }
 
-loginBtn.addEventListener('click', loginAcc);
-
-function loginAcc() {
-  if (loginCheck())
-    showHome(username);
+function handleActiveNavLink(linksArr, link) {
+    linksArr.forEach(l => toggleCssClass(l, 'active', false));
+    toggleCssClass(link, 'active', true);
 }
 
-logoutBtn.addEventListener('click',function() {
-emailLogin.value='';
-passwordLogin.value='';
-  showLogin();
+
+async function getWeather(searchLocation) {
+    try {
+        let response = await fetch(`${baseURL}?key=${APIKey}&q=${searchLocation}&days=3`);
+        if (response.ok && response.status === 200) { return await response.json(); }
+    } catch (e) {
+        console.log("An error occurred:", e);
+    }
+}
+
+
+async function displayWeather() {
+    weatherList = await getWeather(searchLocation);
+    if (!weatherList) { return; }
+    let currentObj = weatherList.current;
+    let locationObj = weatherList.location;
+    let forecastArr = weatherList.forecast.forecastday;
+    let weatherContainer = `
+       <!--^ today -->
+      <div class="col-lg-4">
+          <div class=" forecast today rounded  rounded-3">
+              <div id="today" class="forecast-header px-3  d-flex justify-content-between">
+                  <div class="day fw-semibold">${handleDay(locationObj.localtime)}</div>
+                  <div class="date text-light">${new Date(locationObj.localtime.split(' ')[0]).getDay()} ${handleMonth(locationObj.localtime.split(' ')[0])}</div>
+                  </div>
+                  <div id="current" class="forecast-content  px-3">
+                  <div class="location fs-4">${locationObj.country},${locationObj.name}</div>
+                  <div class="time me-auto">update: ${locationObj.localtime.split(' ')[1]}</div>
+                  <div class="degree d-flex justify-content-between mt-2">
+                      <div class="num">${currentObj.temp_c}<sup>o</sup>C</div>
+                      <div class="forecast-icon align-self-end">
+                          <img src=${currentObj.condition.icon} alt="" width="90">
+                      </div>
+                  </div>
+                  <p class="custom fs-5">${currentObj.condition.text}</p>
+                  <div class=" mt-4 d-flex justify-content-between">
+                      <span class="fs-5"><img class="me-2" src="./assets/img/icon-umberella.png"
+                              alt="">${currentObj.wind_degree}%</span>
+                      <span class="fs-5"><img class="me-2" src="./assets/img/icon-wind.png"
+                              alt="">${currentObj.wind_kph}km/h</span>
+                      <span class="fs-5"><img class="me-2" src="./assets/img/icon-compass.png"
+                              alt="">${currentObj.wind_dir}</span>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <!--^ tomorrow -->
+      <div id="tomorrow" class="col-lg-4 mt-3 mt-lg-0">
+          <div class="text-center h-100 rounded-2 forecast tomorrow">
+              <div class="forecast-header fw-semibold px-3">
+                  <div class="day">${handleDay(forecastArr[1].date)}</div>
+              </div>
+              <div class="forecast-content px-3">
+                  <div class="forecast-icon">
+                      <img src=${forecastArr[1].day.condition.icon} alt="weather icon"
+                          width="90">
+                  </div>
+                  <div class="degree fs-2">${forecastArr[1].day.maxtemp_c}<sup>o</sup>C</div>
+                  <p class="fs-5 mb-0">${forecastArr[1].day.mintemp_c}<sup>o</sup></p>
+                  <p class="custom fs-5">${forecastArr[1].day.condition.text}</p>
+              </div>
+          </div>
+      </div>
+      <!--^ afterTomorrow -->
+      <div id="afterTomorrow" class="col-lg-4 mt-3 mt-lg-0">
+          <div class="text-center h-100 rounded-2 forecast">
+              <div class="forecast-header fw-semibold px-3">
+                  <div class="day">${handleDay(forecastArr[2].date)}</div>
+              </div>
+              <div class="forecast-content px-3">
+                  <div class="forecast-icon">
+                      <img src=${forecastArr[1].day.condition.icon} alt="weather icon"
+                          width="90">
+                  </div>
+                  <div class="degree fs-2">${forecastArr[2].day.maxtemp_c}<sup>o</sup>C</div>
+                  <p class="fs-5 mb-0">${forecastArr[2].day.mintemp_c}<sup>o</sup></p>
+                  <p class="custom fs-5">${forecastArr[2].day.condition.text}</p>
+              </div>
+          </div>
+      </div>
+  `
+    forecast.innerHTML = weatherContainer;
+}
+
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        handleActiveNavLink(navLinks, e.target.parentNode);
+    });
 });
 
-function signUpCheck() {
-  var checkResult = true;
-
-  if (nameSignUp.value == '' || nameSignUp.value == '' || nameSignUp.value == '') {
-    checkResult = false;
-    signUpMsg.innerHTML = `<span style="color: #DC3541;">All inputs are required</span>`
-  }
-
-  for (var i = 0; i < emailsArr.length; i++) {
-    if (emailSignUp.value == emailsArr[i].aEmail) {
-      checkResult = false;
-      signUpMsg.innerHTML = `<span style="color: #DC3541;">email already exists</span>`
-      break;
+btnFind.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (findInput.value) {
+        searchLocation = findInput.value;
+        displayWeather();
     }
-  }
-
-  return checkResult
-}
-
-function loginCheck() {
-
-
-  if (emailLogin.value == '' || passwordLogin.value == '') {
-    loginMsg.innerHTML = `<span style="color: #DC3541;">All inputs are required</span>`
-    return false;
-  }
-
-  for (var i = 0; i < emailsArr.length; i++) {
-    if (emailLogin.value == emailsArr[i].aEmail && passwordLogin.value == emailsArr[i].aPass) {
-      username=emailsArr[i].aName;
-      return true;
+});
+findInput.addEventListener('keyup', (e) => {
+    e.preventDefault();
+    if (findInput.value) {
+        searchLocation = findInput.value;
+        displayWeather();
     }
-  }
-
-  loginMsg.innerHTML = `<span style="color: #DC3541;">incorrect email or password</span>`
-return false;
-
-}
+});
